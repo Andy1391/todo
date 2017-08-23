ee99d61d160b4806a6990d71a04f9517942f8bdf
andy <dp130291mav@gmail.com>
2017-08-23 14:15:29 +0300

    dobav stiley

--- db/migrate/20170816192014__create_projects.rb
+++ db/migrate/20170816192014__create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration
   def change
   	create_table :projects do |t|
-  		t.string :project name
+  		t.string :project_name
 		t.string :title
 		t.string :name
 	 	
